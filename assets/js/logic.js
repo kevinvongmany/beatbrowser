950faf3f116bd2eb19a264b3002f5e6abf25c3f6
Leon <tranhoangquan44@gmail.com>
2024-07-15 20:51:31 +1000

    add logic.js and link script.js

---
+++ assets/js/logic.js
@@ -0,0 +1,5 @@
+// When I clicked search button, I want to get 5 different results song from the Spotify API
+// Then have an option to "see more" to get more results, at least 5 more songs from the Spotify API
+// and then after that, have an option to "see more" and 10 more different songs from the Spotify API but replace 10 previous songs
+// Note: keep the same format for the results, just replace the songs.
+// 
